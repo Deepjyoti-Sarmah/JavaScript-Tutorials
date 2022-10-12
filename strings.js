ee17db4684e6a8e5ad12dbe2451dd3608ca43f34
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-10-12 18:52:17 +0530

    String methods , Set, Map, Closure, Higher order function

---
+++ strings.js
@@ -0,0 +1,40 @@
+var str = "iNeuron is located is Mars";
+
+// var a = str.length;
+// var a = str.toUpperCase();
+// var a = str.toLowerCase();
+// var a = str.includes("is");
+// var a = str.endsWith("s");
+
+// var a = str.search("is"); //8
+// var a = str.search("isa"); //-1
+
+// var a =str.match(/is/g); //[ 'is' , 'is' ]
+// var a =str.match(/is/); 
+// [
+//   'is',
+//   index: 8,
+//   input: 'iNeuron is located is Mars',
+//   groups: undefined
+// ]
+
+// var a = str.charCodeAt(14); //97
+// var a = String.fromCharCode(65); //A
+
+// var str1 = " hi";
+// var str2 = " Deepj";
+// var c = str.concat(str1, str2);
+
+// var a = "Javascript   ";
+// var c = a.trim();
+
+var c= str.slice(-1); //s
+
+// var c = str.substring(2,5);  which index want , from where dont want
+
+// var c = str.substr(2,5); //euron
+
+var c = str.split(" ");
+
+// console.log(a);
+console.log(c);
