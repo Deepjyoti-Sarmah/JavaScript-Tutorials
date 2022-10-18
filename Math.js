8381c7a497f8da30e931dc4ec3ca497e34837033
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-10-18 19:04:01 +0530

    Math,Regex,Asynchronous

---
+++ Math.js
@@ -0,0 +1,15 @@
+// let result = Math.ceil(4.1);
+// let result = Math.floor(-3.9);
+// let result = Math.round(-5.6);
+// let result = Math.trunc(3.4);
+// let result = Math.max(20,30,40,70,50);
+// let result = Math.min(20,30,40,60,70,10);
+// let result = Math.sqrt(13);
+// let result = Math.cbrt(8);
+// let result = Math.pow(2,16);
+// let result = Math.abs(-4.2)
+// let result = Math.PI;
+// let result = Math.random();
+
+
+console.log(result);
---
+++ regex.js
@@ -0,0 +1,11 @@
+// var a = "9INEURON";
+var a = "abcdefg6099346034abcdefg"
+var regex = /^[6-9]\d{9}/ ;
+
+if(regex.test(a))
+{
+    console.log("True");
+}
+else {
+    console.log("False");
+} 
