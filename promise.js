e1e64842327e9bbd36850fb8ec4974f2075db72f
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-10-19 23:15:18 +0530

    promise chaining

--- promise.js
+++ promise.js
@@ -2,28 +2,27 @@
 //this is the executor function aka the promise
 let promise  = new Promise(function(resolve , reject ) {
     //pretend it takes time to bring water
-    // setTimeout(function(){
-    //     //success condition 
-    //     resolve("Hurray!Jack and Jill Back with water");
-    // }, 2000);
-
-    setTimeout(function() {
-        reject(
-            //throwing an error
-            new Error("jack fall down and broke his crown, and jill came trumbling after")
-        );
+    setTimeout(function(){
+        //success condition 
+        resolve("Hurray!Jack and Jill Back with water");
     }, 2000);
+
+    // setTimeout(function() {
+    //     reject(
+    //         //throwing an error
+    //         new Error("jack fall down and broke his crown, and jill came trumbling after")
+    //     );
+    // }, 2000);
 
 });
 
 //consumer function the one which will receive promise
 const grandParentCooking = () => {
     //then - will indicate promise has been fullfilled
-    // promise.then(function(result){
-    //     console.log(`Cooking the veges with the ${result}`);
-    // });
-
-    promise.catch(function(error){
+    //promise chaining 
+    promise.then(function(result){
+        console.log(`Cooking the veges with the ${result}`);
+    }).catch(function(error){
         console.log(`omg ${error.message}`);
     });
 };
