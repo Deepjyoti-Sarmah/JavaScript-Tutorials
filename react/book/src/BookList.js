ffaf645a2513086d978c434c5905037534dda8ee
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2022-11-06 18:46:36 +0530

    props and maps

---
+++ react/book/src/BookList.js
@@ -0,0 +1,29 @@
+export const BookList = [
+    {
+      id:1,
+      Images : "https://m.media-amazon.com/images/I/41xsqjWS4FL._SX328_BO1,204,203,200_.jpg",
+      Title : "Blockchain Basics: A Non-Technical Introduction in 25 Steps",
+      Author : "Daniel Drescher"
+    },
+
+    {
+      id:2,
+      Images : "https://m.media-amazon.com/images/I/51Y2i1M+a3L._SX598_BO1,204,203,200_.jpg",
+      Title : "The Potentialist I: Your Future in the New Reality",
+      Author : "Daniel Drescher"
+    },
+
+    {
+      id:3,
+      Images : "https://m.media-amazon.com/images/I/51pWybDxARL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+      Title : "Blockchain Investing; Bitcoin, Cryptocurrency, NFT, DeFi, Metaverse, Smart Contracts,",
+      Author : "Daniel Drescher"
+    },
+
+    {
+      id:4,
+      Images : "https://m.media-amazon.com/images/I/41efo6NJwEL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      Title : "Mastering Blockchain: A deep dive into distributed ledgers",
+      Author : " Imran Bashir"
+    }
+  ];
